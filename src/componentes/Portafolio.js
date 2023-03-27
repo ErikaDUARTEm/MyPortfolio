975ac188bd8e27a0c40755547fff2da5a10a8aa2
Erika Duarte <erika.duarte.mora@gmail.com>
2023-03-27 16:13:55 -0500

    confg

---
+++ src/componentes/Portafolio.js
@@ -0,0 +1,48 @@
+// export const Portafolio = ()=>{
+//     const headerPortafolio=`
+//      <div>
+//        <header>
+//         <section class='header'>
+//          <article class='logo'>
+//        <img src="./logo.svg" class='img1'/>
+//            </article>
+//          <article class='links'>
+//            <nav>
+//              <ul>
+//                <li class='menu'>
+//                  <a href='#'>Home</a>
+//                  <a href='#Aboutme'>About me</a>
+//                  <a href='#projects'>Projects</a>
+//                  <a href='#contact'>Contact</a>
+//                </li>
+//              </ul>
+//          </nav>
+//        </article>
+//      </section>
+//     </header>
+//     <section class='mensaje'>
+//       <article class='container rgb'>
+//       <div class="foto">
+//       <img src="./img/Erikaperfil.jpg"/>
+//       </div>
+//       <div class="welcome">
+//       <h1>Welcome!</h1>
+//       <h3 class='text'>Thanks for coming to meet me...</h3>
+//       <p class='text'>I am a front-end web developer. </p>
+//       <p class='text'>I invite you to continue exploring my website and to know 
+//       some of my projects, as well as the technologies I have used.</p>
+//       </div>
+//       </article>
+//       <button id='flecha' class="flechas">
+//       <img src="https://img.icons8.com/nolan/64/double-down.png" class='flecha'/>
+//       </button>
+//     </section>
+//    <section class='aboutme'>
+
+//    </section>
+//    </div>
+//    `;
+//    
+
+//  return headerPortafolio;
+// }
